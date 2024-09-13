d36b23bcd3483c144ac3ac746aa075088359619f
Wenjie Cheng <w.cheng-1@student.utwente.nl>
2024-09-13 14:39:46 +0200

    Documentation

--- life.py
+++ life.py
@@ -1,8 +1,19 @@
 from bitarray import bitarray
 
 class Life:
-
+    """A python implementation of Game of Life
+    
+    part of BGP
+    """
     def __init__(self, filename):
+        """Create a game of life object
+    
+        Args:
+            filename: Input file name.
+    
+        Raises:
+            Exceptions: If input data is invalid.
+        """
         self.filename = filename  
         self.grid = []
         with open(filename) as f:
@@ -25,28 +36,23 @@ class Life:
                 self.grid[y + 1][x + 1] = 1
 
     
-    def tick_grid(self):
+    def tick_grid(self,n):
+        """Applies the rules of Game of Life for specified number of generations.
     
-        # prev = bitarray (w+2)
+        Args:
+        n: Number of generations
     
-        for y,row in enumerate(self.grid[1:-1]):
-            y1,y2 = y+1,y+2   
-            curr = bitarray (self.w+2)
-            for x,cell in enumerate(row[1:-1]):
-                count = self.grid[y][x] + self.grid[y][x+1] + self.grid[y][x+2] + self.grid[y1][x] + self.grid[y1][x+2]  + self.grid[y2][x] + self.grid[y2][x+1] + self.grid[y2][x+2]
-                curr[x+1] = 1 if count == 3 or (cell and count ==2) else 0
-    
-            if y > 0:
-                self.grid [y] = prev
-            prev = curr
-        self.grid [y+1] = curr
-                
-        # return grid
-
-
-# filename = 'life.txt'
-# grid = read_grid(filename)
-# print(grid)
-
-# next_grid = tick_grid(grid)
-# print(grid)
+        
+        """
+        for i in range(n):
+            for y,row in enumerate(self.grid[1:-1]):
+                y1,y2 = y+1,y+2   
+                curr = bitarray (self.w+2)
+                for x,cell in enumerate(row[1:-1]):
+                    count = self.grid[y][x] + self.grid[y][x+1] + self.grid[y][x+2] + self.grid[y1][x] + self.grid[y1][x+2]  + self.grid[y2][x] + self.grid[y2][x+1] + self.grid[y2][x+2]
+                    curr[x+1] = 1 if count == 3 or (cell and count ==2) else 0
+        
+                if y > 0:
+                    self.grid [y] = prev
+                prev = curr
+            self.grid [y+1] = curr
