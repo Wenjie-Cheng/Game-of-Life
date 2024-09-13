6738c5bd6ac5d337aebd2e5e6da848c4e519b11c
Wenjie Cheng <w.cheng-1@student.utwente.nl>
2024-09-13 14:26:47 +0200

    convert to function

--- life.py
+++ life.py
@@ -1,66 +1,52 @@
-def read_grid(filename):
-    grid = []
-    with open(filename) as f:
-        
-        w,h = map(int, f.readline().split(' ', 1))
-        
-        for y in range(h):
-            grid.append([0] * w)
-            # grid.append(bytearray(10)) # save memory, bitarray is also avaliable
+from bitarray import bitarray
+
+class Life:
+
+    def __init__(self, filename):
+        self.filename = filename  
+        self.grid = []
+        with open(filename) as f:
             
-        for no, line in enumerate(f):
-            try:
-                y , x = map(int, line.split(' ', 1))
+            self.w,self.h = map(int, f.readline().split(' ', 1))
+            
+            for y in range(self.h + 2):
+                self.grid.append(bitarray(self.w + 2)) # save memory, bitarray is also avaliable
                 
-                if y < 0 or x < 0:
-                    raise ValueError
+            for no, line in enumerate(f):
+                try:
+                    y , x = map(int, line.split(' ', 1))
+                    
+                    if y < 0 or x < 0:
+                        raise ValueError
+    
+                except ValueError:
+                    raise Exception(f"Invaild cell on line {no+2}.")
+     
+                self.grid[y + 1][x + 1] = 1
 
-            except ValueError:
-                raise Exception(f"Invaild cell on line {no+2}.")
-
-
-            grid[y][x] = 1
-    return grid
-
-@profile
-def tick_grid(grid):
-    h,w = len(grid), len(grid[0])
-
-    nextgrid = []
-    for y in range(h):
-        nextgrid.append([0] *w)
-        # nextgrid.append(bytearray(10))
-        
-    for y,row in enumerate(grid):
-        for x,cell in enumerate(row):
-            count = 0
+    
+    def tick_grid(self):
+    
+        # prev = bitarray (w+2)
+    
+        for y,row in enumerate(self.grid[1:-1]):
+            y1,y2 = y+1,y+2   
+            curr = bitarray (self.w+2)
+            for x,cell in enumerate(row[1:-1]):
+                count = self.grid[y][x] + self.grid[y][x+1] + self.grid[y][x+2] + self.grid[y1][x] + self.grid[y1][x+2]  + self.grid[y2][x] + self.grid[y2][x+1] + self.grid[y2][x+2]
+                curr[x+1] = 1 if count == 3 or (cell and count ==2) else 0
+    
             if y > 0:
-                count += grid[y-1][x-1] if x > 0 else 0
-                count += grid[y-1][x]
-                count += grid[y-1][x+1] if x<w-1 else 0
-
-            count += grid[y][x-1] if x > 0 else 0
-            count += grid[y][x+1] if x < w-1 else 0
-
-            if y < h-1:
-                count += grid[y+1][x-1] if x > 0 else 0
-                count += grid[y+1][x]
-                count += grid[y+1][x+1] if x < w-1 else 0
-
-            # if cell:           
-            #     nextgen = 1 if count == 2 or count == 3 else 0
-            # else:    
-            #     nextgen = 1 if count == 3 else 0
-
-            nextgrid[y][x] = 1 if count == 3 or (cell and count == 2) else 0
-
-            # print(y, x, cell, count, nextgen)
-    return nextgrid
+                self.grid [y] = prev
+            prev = curr
+        self.grid [y+1] = curr
+                
+        # return grid
 
 
-filename = 'life.txt'
-grid = read_grid(filename)
-print(grid)
+# filename = 'life.txt'
+# grid = read_grid(filename)
+# print(grid)
 
-next_grid = tick_grid(grid)
-print(grid)
+# next_grid = tick_grid(grid)
+# print(grid)
