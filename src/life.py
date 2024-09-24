7555bc942ad0585a5e070c6abcc9be55bbdf0687
Wenjie Cheng <w.cheng-1@student.utwente.nl>
2024-09-24 18:51:32 +0200

    Adjust grid size

--- src/life.py
+++ src/life.py
@@ -15,13 +15,13 @@ class Life:
             Exceptions: If input data is invalid.
         """
         self.filename = filename  
-        self.grid = []
+        self.extgrid = []
         with open(filename) as f:
             
             self.w,self.h = map(int, f.readline().split(' ', 1))
             
             for y in range(self.h + 2):
-                self.grid.append(bitarray(self.w + 2)) # save memory, bitarray is also avaliable
+                self.extgrid.append(bitarray(self.w + 2)) # save memory, bitarray is also avaliable
                 
             for no, line in enumerate(f):
                 try:
@@ -33,7 +33,9 @@ class Life:
                 except ValueError:
                     raise Exception(f"Invaild cell on line {no+2}.")
      
-                self.grid[y + 1][x + 1] = 1
+                self.extgrid[y + 1][x + 1] = 1
+
+        self.grid = [row[1:-1] for row in self.extgrid[1:-1]]
 
     
     def tick_grid(self,n:int=1):
@@ -43,14 +45,16 @@ class Life:
         n: Number of generations      
         """
         for i in range(n):
-            for y,row in enumerate(self.grid[1:-1]):
+            for y,row in enumerate(self.extgrid[1:-1]):
                 y1,y2 = y+1,y+2   
                 curr = bitarray (self.w+2)
                 for x,cell in enumerate(row[1:-1]):
-                    count = self.grid[y][x] + self.grid[y][x+1] + self.grid[y][x+2] + self.grid[y1][x] + self.grid[y1][x+2]  + self.grid[y2][x] + self.grid[y2][x+1] + self.grid[y2][x+2]
+                    count = self.extgrid[y][x] + self.extgrid[y][x+1] + self.extgrid[y][x+2] + self.extgrid[y1][x] + self.extgrid[y1][x+2]  + self.extgrid[y2][x] + self.extgrid[y2][x+1] + self.extgrid[y2][x+2]
                     curr[x+1] = 1 if count == 3 or (cell and count ==2) else 0
         
                 if y > 0:
-                    self.grid [y] = prev
+                    self.extgrid [y] = prev
                 prev = curr
-            self.grid [y+1] = curr
+            self.extgrid [y+1] = curr
+
+        self.grid = [row[1:-1] for row in self.extgrid[1:-1]]
